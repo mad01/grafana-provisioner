8f32b245294a8092f7e88d8dca50522cf3252d0e
Alexander Brandstedt <alexander@infralium.com>
2018-06-20 14:49:07 +0200

    init db stuff

--- main.go
+++ main.go
@@ -2,6 +2,34 @@ package main
 
 import "fmt"
 
+var configData = `
+dnsBase: example.com
+databaseURL: username:password@example.com/dashboards
+`
+
+var grafanasData = `
+teams:
+  - foo
+  - bar
+  - baz
+`
+
+type TeamsConfig struct {
+	teams []string `yaml:teams`
+}
+
+type DatabaseConfig struct {
+	dnsBase     string `yaml:"dnsBase"`
+	databaseURL string `yaml:"databaseURL"`
+}
+
+type DB struct{}
+
+func (d *DB) connect()  {}
+func (d *DB) createDB() {}
+func (d *DB) removeDB() {}
+func (d *DB) listDB()   {}
+
 func main() {
 	err := runCmd()
 	if err != nil {
