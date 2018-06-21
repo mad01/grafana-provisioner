1f7bdaaed95494439c3b86776bed057f54b02b2b
Alexander Brandstedt <alexander@infralium.com>
2018-06-21 11:41:11 +0200

    starting work on provision cmd

--- cmd.go
+++ cmd.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/mad01/grafana-provisioner/pkg/kubectl"
 	"github.com/spf13/cobra"
 )
 
@@ -18,9 +19,53 @@ func cmdVersion() *cobra.Command {
 	return command
 }
 
+func cmdProvision() *cobra.Command {
+	var kubeconfig, dbURL, team string
+	var command = &cobra.Command{
+		Use:   "provision",
+		Short: "provision grafana and db",
+		Long:  "",
+		Run: func(cmd *cobra.Command, args []string) {
+			db := DB{
+				URL: dbURL,
+			}
+			db.connect()
+			db.createDB(team)
+
+			values := manifestValues{}
+			values.databaseURL = fmt.Sprintf("%s/%s", dbURL, team)
+
+			values.ingressClass = "nginx"
+			values.ingressHost = fmt.Sprintf("%s.example.com")
+			values.ingressName = fmt.Sprintf("grafana-%s", team)
+
+			values.serviceName = fmt.Sprintf("grafana-%s", team)
+			values.namespace = team
+			values.image = "grafana/grafana:5.1.0"
+
+			values.deploymentName = fmt.Sprintf("grafana-%s", team)
+			values.deploymentLabelKey = "app"
+			values.deploymentLabelValue = fmt.Sprintf("grafana-%s", team)
+
+			manifest := manifestRender(values)
+
+			kubectl := kubectl.NewKubectlClient(kubeconfig)
+			kubectl.Apply(manifest)
+
+		},
+	}
+
+	command.Flags().StringVarP(&kubeconfig, "kube.config", "k", "", "outside cluster path to kube config")
+	command.Flags().StringVarP(&dbURL, "db.url", "u", "", "mysql database url")
+	command.Flags().StringVarP(&team, "team", "t", "foo", "teamname")
+
+	return command
+}
+
 func runCmd() error {
 	var rootCmd = &cobra.Command{Use: "grafana-provisioner"}
 	rootCmd.AddCommand(cmdVersion())
+	rootCmd.AddCommand(cmdProvision())
 
 	err := rootCmd.Execute()
 	if err != nil {
