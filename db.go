abacfef6e0e9e10f9817cf8f24162a849de4a50b
Alexander Brandstedt <alexander@infralium.com>
2018-06-25 11:54:46 +0200

    move out db funcs to separate file

---
+++ db.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type DB struct {
+	URL  string
+	conn *sql.DB
+}
+
+func (d *DB) connect() error {
+	db, err := sql.Open("mysql", d.URL)
+	if err != nil {
+		return err
+	}
+	d.conn = db
+	fmt.Printf("connected to database: %v\n", d.URL)
+	return nil
+}
+
+func (d *DB) createDB(name string) error {
+	_, err := d.conn.Exec("CREATE DATABASE IF NOT EXISTS " + name)
+	return err
+}
--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-
-	_ "github.com/go-sql-driver/mysql"
 )
 
 var grafanasData = `
@@ -21,26 +18,6 @@ type TeamsConfig struct {
 type DatabaseConfig struct {
 	dnsBase     string `yaml:"dnsBase"`
 	databaseURL string `yaml:"databaseURL"`
-}
-
-type DB struct {
-	URL  string
-	conn *sql.DB
-}
-
-func (d *DB) connect() error {
-	db, err := sql.Open("mysql", d.URL)
-	if err != nil {
-		return err
-	}
-	d.conn = db
-	fmt.Printf("connected to database: %v\n", d.URL)
-	return nil
-}
-
-func (d *DB) createDB(name string) error {
-	_, err := d.conn.Exec("CREATE DATABASE IF NOT EXISTS " + name)
-	return err
 }
 
 func main() {
